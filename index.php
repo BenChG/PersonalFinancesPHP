2d3183be59488eb19e03ec7bc60f48c7a4090c75
username <example@example.com>
2021-01-14 06:32:43 +0100

    Added logging forms

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	
-	<title>Strona główna</title>
+	<title>Main page</title>
 	
 	<meta name="description" content="Personal Finances - the best way to manage and save your money" />
 	<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
@@ -13,6 +13,13 @@
 
 <body>
 	
+	 <form action="login.php" method="post">
+			<input type="text" name="login"/>
+		    <br/>
+		    <input type="password" name="password"/>
+		    <br/>
+		    <button type="submit">log in</button>
+	 </form>
 	
 </body>
 </html>
