942fc52ea6cc0214cab317661da3df200c5254e7
username <example@example.com>
2021-01-14 06:21:10 +0100

    Initial check-in

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	
+	<title>Strona główna</title>
+	
+	<meta name="description" content="Personal Finances - the best way to manage and save your money" />
+	<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
+	
+</head>
+
+<body>
+	
+	
+</body>
+</html>
