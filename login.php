c786816e9611ef99b6b4cd75c2d47d76defff04e
username <example@example.com>
2021-01-18 07:23:39 +0100

    Added feature incorrect login or password and session run

--- login.php
+++ login.php
@@ -22,17 +22,20 @@ require_once "connect.php";
 			$how_many_users = $result->num_rows;
 			if ($how_many_users>0)
 			{
+				$_SESSION['loggedin'] = true;
 				$row = $result->fetch_assoc();
 				$_SESSION['id']  = $row['id'];
 				$_SESSION['username']  = $row['username'];
 				$_SESSION['password']  = $row['password'];
 				$_SESSION['email']  = $row['email'];
 				
+				unset($_SESSION['error']);
 				$result->free_result();
 				header('Location: main_page.php');
 				
 			} else {
-				
+						$_SESSION['error'] = '<span style="color:red">Incorrect login or password !</span>';
+					header('Location: index.php');	
 				
 				
 			}
