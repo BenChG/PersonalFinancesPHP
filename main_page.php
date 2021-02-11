2b2fea63cb7089393b790a56d7637ebe15d79ac2
username <example@example.com>
2021-02-11 05:03:08 +0100

    Implemented PF html into pages index,main_page, signin, and main.css file

--- main_page.php
+++ main_page.php
@@ -1,6 +1,6 @@
 <?php
-
-session_start();
+	
+	session_start();
 	
 	if(!isset($_SESSION['loggedin']))
 	{
@@ -12,34 +12,105 @@ session_start();
 
 <!DOCTYPE HTML>
 <html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<link rel="stylesheet" href="main.css" type="text/css" />
+	<head>
+		
+		<meta charset="utf-8">
+		<title>Main Page</title>
+		<meta name="description" content="Personal Finances - the best way to manage and save your money" />
+		<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
+		<meta name="author" content="BCG">
+		<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+		<link rel="stylesheet" href="main.css" type="text/css" />
+		<link href "https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Lato&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/fontello.css" type="text/css" />
+		
+	</head>
 	
-	<title>Main page</title>
+	<body>
 	
-	<meta name="description" content="Personal Finances - the best way to manage and save your money" />
-	<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
-	
-</head>
-
-<body>
-	
-	<?php
+		<?php
 	
 	echo "<p>Hello " .$_SESSION['username'].'! [ <a href="logout.php" class="link">Log out!</a>]</p>';
 	
 	?>
 		
-			<a href="add_income.php" class="main_page">Add new income </a>
-			<br /><br /> 
-
-			<a href="add_expense.php" class="main_page">Add new expense</a>
-			<br /><br /> 
+		<div id="container">
+			
+			<div class="tile_icon_balance">
+				<a href="index.html" class="link"><i class="icon-home2"></i></a>	
+			</div>
+			
+			<div class="header_main_menu">
+				Personal Finances 
+			</div>
+			
+			<div style="margin-left:-6px;">	
+				<div class="tile_menu">	
+					
+					<div class="tile_icon_menu" style="background-color: #33CC33;">	
+						<a href="add_income.php" class="link"><i class="icon-list-add"></br><h1>Add new income</h1></i></a>	
+					</div>	
+					
+				</div>	
+			</div>
+			
+			<div class="tile_menu">
+				<div class="tile_icon_menu" style="background-color: #FF3333;">	
+					<a href="add_expense.php" class="link"><i class="icon-list-add"></br><h1>Add new expense</h1></i></a>
+				</div>
+			</div>		
+			
+			<div class="tile_menu">
+				<div class="tile_icon_menu"style="background-color: #7D3C98;" >	
+					<a href="password.php" class="link"><i class="icon-key"></br><h1>Change Password</h1></i></a>
+				</div>
+			</div>		
+			
+			<div style="margin-left:-6px;">
+				<div class="tile_menu">	
+					<div class="tile_icon_menu" style="background-color:#D35400;">	
+						<a href="show_balance.php" class="link"><i class="icon-balance-scale"></br><h1>Current month</h1></i></a>	
+					</div>	
+				</div>	
+			</div>	
+			
+			<div class="tile_menu">
+				<div class="tile_icon_menu" style="background-color:#707B7C">
+					<a href="show_balance.php" class="link"><i class="icon-balance-scale"></br><h1>Previous month</h1></i></a>
+				</div>
+			</div>		
+			
+			<div class="tile_menu">
+				<div class="tile_icon_menu" style="background-color:#2E86C1;">	
+					<a href="show_balance.php" class="link"><i class="icon-balance-scale"></br><h1>Selected period</h1></i></a>
+				</div>
+			</div>		
+			
+			<div style="clear:both;"></div>		
+			
+		</body>
+		
+		<footer>
+			
+			<div class="long_tile">	
+				<div id="email"> Contact me via mail:  
+					<a href="mailto:ben.chalubinski@gmail.com target="_blank" class="link">
+						<i class="icon-mail"></i> 
+					</a>			
+				</div>
+			</div>
+			
+			
+			<div class="long_tile">	
+				<div id="rights">
+					2020 &copy;  Personal Finances  
+					</br> 
+					by Benedykt Chałubiński-Gonerko	
+				</div>
+			</div>
+			
+		</div>		
+		
+	</footer>  
 	
-			<a href="show_balance.php" class="main_page">Show the balance</a>
-			<br /><br /> 
-	
-</body>
 </html>
--- signin.php
+++ signin.php
@@ -1,14 +1,26 @@
+<?php
+	
+	session_start();
+	
+	if ((isset($_SESSION['loggedin'])) && ($_SESSION['loggedin']==true))
+	{
+		header('Location: main_page.php');
+		exit();
+	}
+	
+?>
+
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
 		
-		<meta charset="utf-8"/>
-		<title>Main page</title>
+		<meta charset="utf-8">
+		<title>Personal Finances</title>
 		<meta name="description" content="Personal Finances - the best way to manage and save your money" />
 		<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
 		<meta name="author" content="BCG">
 		<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
-		<link rel="stylesheet" href="main.css" type="text/css" />
+		<link rel="stylesheet" href="main.css">
 		<link href "https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Lato&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="css/fontello.css" type="text/css" />
 		
@@ -20,26 +32,32 @@
 			
 			<div class="header">
 				Personal Budget
-			</div>
-			
-			<div class="tile_left">	
-				<div class="tile_icon">	
-					<a href="signin.php" target="_blank" class="link">
-						<i class="icon-edit-1"></br> Log in - control</br> your budget! </i>
-					</a>	
-				</div>	
 			</div>	
+			
+			<div class="tile_left">
+				<form action="login.php" method="post">	
+					<input type="text" name="login" placeholder="login" onfocus="this.placeholder=''" onblur="this.placeholder='login'">
+					</br>
+					<input type="password" name="password" placeholder="password" onfocus="this.placeholder=''" onblur="this.placeholder='password'">
+					</br>
+					<input type="submit" value="Sign in">		
+				</form>
+			</div>
 			
 			<div class="tile_right">
 				<div class="tile_icon">
-					<a href="registration.php" class="link">
-						<i class="icon-login-1"></br>Registration - create </br> account for free!</i>
-					</a>
+					<a href="index.php" target="_blank" class="link">
+						<i class="icon-home"></br>Home</i>
+					</a>	
 				</div>
 			</div>	
 			
-			<div style="clear:both;"></div>		
-					
+			<div style="clear:both;"></div>
+			
+			<?php
+				if(isset($_SESSION['error'])) echo $_SESSION['error'];
+			?>
+			
 		</body>
 		
 		<footer>
@@ -55,8 +73,7 @@
 			<div class="long_tile">	
 				<div id="rights">
 					2020 &copy; Personal Finances  
-					</br> 
-					by Benedykt Chałubiński-Gonerko	
+					</br> by Benedykt Chałubiński-Gonerko	
 				</div>
 			</div>
 			
