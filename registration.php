8947db8eb96465014f649e22127f79bca49a901a
username <example@example.com>
2021-02-11 06:35:38 +0100

    Implemented PF html into registration page

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
-
+	
 	session_start();
-
+	
 	if (isset($_POST['email']))
 	{
 		//Success validation
@@ -44,7 +44,7 @@
 			$all_right=false;
 			$_SESSION['e_password']="Provided passwords are not equal!";
 		}
-			
+		
 		$password_hash = password_hash($password1, PASSWORD_DEFAULT);
 		
 		if (!isset($_POST['terms']))
@@ -80,7 +80,7 @@
 			$connection = new mysqli($host, $db_user, $db_password, $db_name);
 			if ($connection->connect_errno!=0)
 			{
-				 throw new Exception(mysqli_connect_errno());
+				throw new Exception(mysqli_connect_errno());
 			}		
 			
 			else
@@ -106,7 +106,7 @@
 					$all_right=false;
 					$_SESSION['e_login']="There is already user with privided login, please choose another one!";
 				}
-						
+				
 				if ($all_right==true)
 				{
 					//Great, all tests passed, we are adding now new user to our database!
@@ -118,39 +118,39 @@
 						$success=true;
 						
 						if($result = $connection->query("SELECT * FROM users WHERE username='$login'"))
-					{
-						$row = $result->fetch_assoc();
-						$id = $row["id"];
-						
-						if ($connection->query("INSERT INTO payment_methods_assigned_to_users VALUES
+						{
+							$row = $result->fetch_assoc();
+							$id = $row["id"];
+							
+							if ($connection->query("INSERT INTO payment_methods_assigned_to_users VALUES
 							(NULL, '$id', 'Cash'), (NULL,'$id', 'Debit Card'), (NULL,'$id', 'Credit Card')"))
 							{
 								$_SESSION['successfully_added_defalut_incomes']=true;
 							}
-						
-						else
+							
+							else
 							{ 
 								$success=false; throw new Exception($connection->error);
 							}
-										
-						if ($connection->query("INSERT INTO incomes_category_assigned_to_users VALUES
+							
+							if ($connection->query("INSERT INTO incomes_category_assigned_to_users VALUES
 							(NULL, '$id', 'Salary'), (NULL,'$id', 'Interest'), (NULL,'$id', 'Allegro'), (NULL,'$id', 'Another')"))
 							{
 								$_SESSION['successfully_added_defalut_incomes']=true;
 							}
-						
-						else
+							
+							else
 							{ 
 								$success=false; throw new Exception($connection->error);
 							}
-			
-						if ($connection->query("INSERT INTO expenses_category_assigned_to_users VALUES (NULL, '$id', 'Transport'), (NULL,'$id', 'Books'), (NULL,'$id', 'Food'), 
+							
+							if ($connection->query("INSERT INTO expenses_category_assigned_to_users VALUES (NULL, '$id', 'Transport'), (NULL,'$id', 'Books'), (NULL,'$id', 'Food'), 
 							(NULL,'$id', 'Apartments'),(NULL, '$id', 'Telecommunication'), (NULL,'$id', 'Health'), (NULL,'$id', 'Clothes'), (NULL,'$id', 'Hygiene'),(NULL, '$id', 'Kids'), (NULL,'$id', 'Recreation'), (NULL,'$id', 'Trip'), (NULL,'$id', 'Savings'),(NULL, '$id', 'For Retirement'), (NULL,'$id', 'Debt Repayment'), (NULL,'$id', 'Gift'), (NULL,'$id', 'Another')"))					
 							{
-									$_SESSION['successfully_added_defalut_expenses']=true;					
+								$_SESSION['successfully_added_defalut_expenses']=true;					
 							}
 							
-						else
+							else
 							{
 								$success=false; throw new Exception($connection->error);
 							}
@@ -161,20 +161,20 @@
 					{ 
 						$success=false; throw new Exception($connection->error);
 					}
-		
-				}
 					
-					else
-					{
-						 throw new Exception($connection->error);
-						 $success=false;
-					}
-								
+				}
+				
+				else
+				{
+					throw new Exception($connection->error);
+					$success=false;
+				}
+				
 			}				
 			
-					$connection->close();	
+			$connection->close();	
 		}
-			
+		
 		catch(Exception $e)
 		{
 			echo '<span style="color:red;"> Server error! We appologize for the inconvenience and ask you to register another day!</span>';
@@ -186,117 +186,194 @@
 
 <!DOCTYPE HTML>
 <html lang="pl">
-<head>
-	 <meta chatset="utf-8"/>
-	 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	 <link rel="stylesheet" href="main.css" type="text/css" />
-	 
-	 <title>Personal finanses - create your own account!</title>
-	 <script src="https://www.google.com/recaptcha/api.js"></script>
-	 
-	 <style>
-	 .error
-	 {
-		 color:red;
-		 margin-top: 10px;
-		 margin-robottom: 10px;
-	 }
-	 </style>
+	<head>
+		<meta chatset="utf-8"/>
+		<title>Personal finanses - create your own account!</title>
+		<meta name="description" content="Personal Finances - the best way to manage and save your money" />
+		<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
+		<meta name="author" content="BCG">
+		
+		<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+		
+		<link rel="stylesheet" href="main.css">
+		
+		<link href "https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Lato&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/fontello.css" type="text/css" />
+		
+		<script src="https://www.google.com/recaptcha/api.js"></script>
+		
+		
+		
+		
+		<style>
+			.error
+			{
+			color:red;
+			margin-top: 10px;
+			margin-robottom: 10px;
+			}
+		</style>
+		
+	</head>
 	
-</head>
-
-<body>
-
-	<form method="post">
+	<body>
+		
+		<div id="container"style="margin-top:120px;">
+			
+			<div class="header">
+				Personal Finances
+			</div>	
+			
+			<form method="post">
+				<div class="tile_left">
+					
+					
+					<input type="text" placeholder="login" 
+					onfocus="this.placeholder=''" onblur="this.placeholder='login'" 
+					value="<?php 
+						if (isset($_SESSION['fr_login'])) 
+						{ 
+							echo $_SESSION['fr_login'];
+							unset($_SESSION['fr_login']);
+						}
+					?>"name="login" /> <br />
+					
+					<?php
+						if (isset($_SESSION['e_login']))
+						{
+							echo '<div class="error">'.$_SESSION['e_login'].'</div>';
+							unset($_SESSION['e_login']);
+						}
+					?>
+					
+					<input type="password" placeholder="password" 
+					onfocus="this.placeholder=''" onblur="this.placeholder='password'"
+					value="<?php 
+						if (isset($_SESSION['fr_password1'])) 
+						{ 
+							echo $_SESSION['fr_password1'];
+							unset($_SESSION['fr_password1']);
+						}
+					?>"name="password1" /> <br />
+					
+					<?php
+						if (isset($_SESSION['e_password']))
+						{
+							echo '<div class="error">'.$_SESSION['e_password'].'</div>';
+							unset($_SESSION['e_password']);
+						}		
+					?>
+					
+					<input type="password" placeholder="password" 
+					onfocus="this.placeholder=''" onblur="this.placeholder='password'"
+					value="<?php
+						if (isset($_SESSION['fr_password2']))
+						{
+							echo $_SESSION['fr_password2'];
+							unset($_SESSION['fr_password2']);
+						}
+					?>"name="password2" /><br />
+					
+					<input type="submit" value="Sign up"/>
+					
+					
+				</div>
+				
+				<div class="tile_right">
+					<div class="tile_icon">
+						<a href="index.html" target="_blank" class="link">
+							<i class="icon-home"></br>Home</i>
+						</a>	
+					</div>
+					
+					<div style="margin-top:10px;">
+						
+						<input type="text" placeholder="e-mail" 
+						onfocus="this.placeholder=''" onblur="this.placeholder='e-mail'"
+						value="<?php 
+							if (isset($_SESSION['fr_email'])) 
+							{ 
+								echo $_SESSION['fr_email'];
+								unset($_SESSION['fr_email']);
+							}
+						?>"name="email" /> <br />
+						
+						<?php
+							if (isset($_SESSION['e_email']))
+							{
+								echo '<div class="error">'.$_SESSION['e_email'].'</div>';
+								unset($_SESSION['e_email']);
+							}
+						?>
+					</div>
+					
+					
+				</div>
+				
+				<div style="clear:both;"></div>
+				
+				<div class="long_tile">	
+					
+					</br>
+					
+					<label style="font-size: 16px;border: 2px solid white; padding: 10px;">
+						<input type="checkbox" name="terms" <?php
+							if (isset($_SESSION['fr_terms']))
+							{
+								echo "checked";
+								unset($_SESSION['fr_terms']);
+							}
+						?>/> Acceptance of terms
+					</label>
+					
+					<?php
+						if (isset($_SESSION['e_terms']))
+						{
+							echo '<div class="error">'.$_SESSION['e_terms'].'</div>';
+							unset($_SESSION['e_terms']);
+						}
+					?>
+					<br/><br>
+					
+					<div style="text-align: center;">
+						
+						<div class="g-recaptcha" data-theme="light" data-sitekey="6Leun-kZAAAAABcq3o2JqENw68xhnw5WAet3Y42d" style="transform:scale(1.00);-webkit-transform:scale(1.00);transform-origin:0 0;-webkit-transform-origin:0 0;display: inline-block;"></div>
+						
+						<?php
+							if (isset($_SESSION['e_robot']))
+							{
+								echo '<div class="error">'.$_SESSION['e_robot'].'</div>';
+								unset($_SESSION['e_robot']);
+							}
+						?>  
+					</div>
+					
+				</div>
+				
+				<div style="clear:both;"></div>									
+			</form>
+		</body>
+		
+		<footer>
+			
+			<div class="long_tile">	
+				<div id="email"> Contact me via mail:  
+					<a href="mailto:ben.chalubinski@gmail.com target="_blank" class="link">
+						<i class="icon-mail"></i> 
+					</a>			
+				</div>
+				</div>
+				
+				<div class="long_tile">	
+					<div id="rights">
+					2020 &copy; Personal Finances  
+					</br> 
+					by Benedykt Chałubiński-Gonerko	
+				</div>
+			</div>
+			
+		</div>		
+		
+	</footer>  
 	
-		Login: <br /> <input type="text" value="<?php 
-		if (isset($_SESSION['fr_login'])) 
-		{ 
-		  echo $_SESSION['fr_login'];
-		  unset($_SESSION['fr_login']);
-		}
-		?>"name="login" /> <br />
-		
-		<?php
-			if (isset($_SESSION['e_login']))
-			{
-				echo '<div class="error">'.$_SESSION['e_login'].'</div>';
-				unset($_SESSION['e_login']);
-			}
-		?>
-		
-		E-mail: <br /> <input type="text" value="<?php 
-		if (isset($_SESSION['fr_email'])) 
-		{ 
-		  echo $_SESSION['fr_email'];
-		  unset($_SESSION['fr_email']);
-		}
-		?>"name="email" /> <br />
-		
-		 <?php
-			if (isset($_SESSION['e_email']))
-			{
-				echo '<div class="error">'.$_SESSION['e_email'].'</div>';
-				unset($_SESSION['e_email']);
-			}
-		?>
-
-		Your password: <br /> <input type="password" value="<?php 
-		if (isset($_SESSION['fr_password1'])) 
-		{ 
-		  echo $_SESSION['fr_password1'];
-		  unset($_SESSION['fr_password1']);
-		}
-		?>"name="password1" /> <br />
-		
-		<?php
-			if (isset($_SESSION['e_password']))
-			{
-				echo '<div class="error">'.$_SESSION['e_password'].'</div>';
-				unset($_SESSION['e_password']);
-			}
-		?>
-		
-			Repeat password: <br /> <input type="password" value="<?php
-			if (isset($_SESSION['fr_password2']))
-			{
-				echo $_SESSION['fr_password2'];
-				unset($_SESSION['fr_password2']);
-			}
-		?>"name="password2" /><br /><br />
-		
-		<label>
-			<input type="checkbox" name="terms" <?php
-			if (isset($_SESSION['fr_terms']))
-			{
-				echo "checked";
-				unset($_SESSION['fr_terms']);
-			}
-			?>/> Acceptance of terms 
-		</label>
-		
-		<?php
-			if (isset($_SESSION['e_terms']))
-			{
-				echo '<div class="error">'.$_SESSION['e_terms'].'</div>';
-				unset($_SESSION['e_terms']);
-			}
-		?>
-		<br /><br />
-		
-		  <div class="g-recaptcha" data-sitekey="6Leun-kZAAAAABcq3o2JqENw68xhnw5WAet3Y42d"></div>
-		  
-		 <?php
-			if (isset($_SESSION['e_robot']))
-			{
-				echo '<div class="error">'.$_SESSION['e_robot'].'</div>';
-				unset($_SESSION['e_robot']);
-			}
-		?>  <br />
-		  
-		  <input type="submit" value="Sign up" />
-		  
-	</form>
-
-</body>
 </html>
