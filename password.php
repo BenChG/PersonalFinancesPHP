f8367a6517987a992e505a4876ae3388fda2b97a
username <example@example.com>
2021-02-22 07:03:58 +0100

    Added page password - under construction

---
+++ password.php
@@ -0,0 +1,56 @@
+<?php
+	
+	session_start();
+	
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		
+		<meta charset="utf-8">
+		<title>Personal Finances</title>
+		<meta name="description" content="Personal Finances - the best way to manage and save your money" />
+		<meta name="keywords" content="finances, personal, budget, money, wallet, save, incomes, expenses, manage" />
+		<meta name="author" content="BCG">
+		<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+		<link rel="stylesheet" href="main.css">
+		<link href "https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Lato&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/fontello.css" type="text/css" />
+		
+	</head>
+	
+	<body>
+		
+		<div id="container">
+			
+			<div class="header" style="height:360px; color:red;">
+			</br></br></br>Page under construction 
+			<div style= "font-size:20px"> Sorry for the inconvenience!!! </div>
+			</div>	
+
+
+		</body>
+		
+		<footer>
+			
+			<div class="long_tile">	
+				<div id="email"> Contact me via mail:  
+					<a href="mailto:ben.chalubinski@gmail.com target="_blank" class="link">
+						<i class="icon-mail"></i> 
+					</a>			
+				</div>
+			</div>
+			
+			<div class="long_tile">	
+				<div id="rights">
+					2021 &copy; Personal Finances  
+					</br> by Benedykt Chałubiński-Gonerko	
+				</div>
+			</div>
+			
+		</div>		
+		
+	</footer>  
+	
+</html>
